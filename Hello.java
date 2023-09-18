2b72d554eb3b79482554b910201912922b1641b3
Jbibay29 <115449790+Joker03292000@users.noreply.github.com>
2023-09-18 17:36:38 +0800

    change some code in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
   public static void main(String [] args){
    System.out.println ("Complicated world of SoftDev that is why it pays much!");
+System.out.println ("That is why developers are highly paid!");
   }
 }
